09f7149ff82f75f0d321b66c4040fd957925d9f2
kstone3-llc <kstone3@lakelandcollege.edu>
2026-09-02 17:54:39 -0500

    Add files via upload

---
+++ Stone-Kolton-javascript-introduction/script.js
@@ -0,0 +1,24 @@
+//Kolton Stone
+//Purpose: to contain statements that allow for trouble shooting.
+
+//Console Log Statments
+console.log("Student name:", "Kolton Stone");
+console.log("Course:", "CIS-156: Introduction to Programming");
+console.log("Previous technology experience:", "CodeHS Python course in high school.");
+console.log("Learning goal:", "To fully understand the basics of JavaScript and application creation.");
+console.log("Application I would like to build:", "A utility application for games that allows for input of data")
+console.log("My JavaScript file is connected.");
+
+//Troubleshooting Statements
+console.warn("This is test warning message.");
+console.error("This is a test error message.");
+
+//Console Table
+const studentInfo = {
+  name: "Kolton Stone",
+  course: "CIS-156: Introduction to Programming"
+};
+console.table(studentInfo);
+
+//Error Investigation
+console.log("This is a test message to investigate errors in the console.");
