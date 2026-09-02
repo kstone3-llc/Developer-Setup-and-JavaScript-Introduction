e93386cfdec6a42f6375deab74b3794160d24060
kstone3-llc <kstone3@lakelandcollege.edu>
2026-09-02 17:54:19 -0500

    Delete Developer-setup directory

--- Developer-setup/script.js
+++
@@ -1,4 +0,0 @@
-console.log("My JavaScript file is connected.");
-console.log("My name is Kolton Stone.");
-console.log("I am learning JavaScript.");
-console.log("This code is running in the browser.");
