3b829f4c5e5fac002ff0261c8641e646324b651f
kstone3-llc <kstone3@lakelandcollege.edu>
2026-09-02 17:45:21 -0500

    Add files via upload

---
+++ Developer-setup/script.js
@@ -0,0 +1,4 @@
+console.log("My JavaScript file is connected.");
+console.log("My name is Kolton Stone.");
+console.log("I am learning JavaScript.");
+console.log("This code is running in the browser.");
